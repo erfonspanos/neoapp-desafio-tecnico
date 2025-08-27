023b2e15f1293eb2d7fc4f7481640628612a57cf
Erfon Spanos <erfonspanos@gmail.com>
2025-08-27 20:44:41 -0300

    Subindo projeto com Swagger ja configurado

---
+++ src/main/java/com/neoapp/desafio/api_clientes/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.neoapp.desafio.api_clientes;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de clientes")
+                        .description("Documentação para API REST para o desafio técnico")
+                        .version("1.0.0"));
+    }
+}
---
+++ src/test/java/com/neoapp/desafio/api_clientes/ApiClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoapp.desafio.api_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
