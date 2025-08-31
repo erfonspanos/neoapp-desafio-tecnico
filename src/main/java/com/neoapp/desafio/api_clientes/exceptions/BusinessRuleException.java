a2a83d9d06e3841de68996ee8e56dc6acc9b24e5
Erfon Spanos <erfonspanos@gmail.com>
2025-08-31 15:25:34 -0300

    serialVersionUID adicionado ao BusinessRuleException

--- src/main/java/com/neoapp/desafio/api_clientes/exceptions/BusinessRuleException.java
+++ src/main/java/com/neoapp/desafio/api_clientes/exceptions/BusinessRuleException.java
@@ -1,6 +1,8 @@
 package com.neoapp.desafio.api_clientes.exceptions;
 
 public class BusinessRuleException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public BusinessRuleException(String msg) {
         super(msg);
     }
