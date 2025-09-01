0d79138ff1ff5442080a7c0ce632dbf4ee859336
erfonspanos <erfonspanos@gmail.com>
2025-09-01 19:57:33 -0300

    UsuarioRepository criado

---
+++ src/main/java/com/neoapp/desafio/api_clientes/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.neoapp.desafio.api_clientes.repositories;
+
+import com.neoapp.desafio.api_clientes.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByClienteId(Long clienteId);
+}
