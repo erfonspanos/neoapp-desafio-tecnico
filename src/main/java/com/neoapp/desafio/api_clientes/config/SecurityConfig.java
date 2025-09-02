badcfd645211502433af4ab75d251ba4c0a361de
erfonspanos <erfonspanos@gmail.com>
2025-09-02 10:42:34 -0300

    AutenticacaoRepository criado

--- src/main/java/com/neoapp/desafio/api_clientes/config/SecurityConfig.java
+++ src/main/java/com/neoapp/desafio/api_clientes/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,14 +14,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                // Desabilita o CSRF, pois a API será stateless (não usará sessões)
-                .csrf(AbstractHttpConfigurer::disable)
-                // Define as regras de autorização
-                .authorizeHttpRequests(authorize -> authorize
-                        // PERMITE ACESSO A TODOS OS ENDPOINTS SEM AUTENTICAÇÃO
-                        .requestMatchers("/**").permitAll()
-                );
-        return http.build();
+        return http
+                .csrf(crf ->  crf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
